99ad35473f0725f4343d79d0ad2e7fbc95723520
MarlexJL <NatalyS@upn.pe>
2025-04-16 16:27:46 -0500

    Ejercici1 culminado

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -10,6 +10,40 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            //ax^2 + bx + c
+            //2 soluciones
+            //disc > 0 -> 2 sol.
+            //disc = 0 -> 1 sol.
+            //disc < 0 -> 0 sol.
+            int a, b, c;
+            double x1, x2, disc;
+            Console.Write("Ingrese valor de a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese valor de c: ");
+            c = int.Parse(Console.ReadLine());
+            disc = (b * b) - (4 * a * c);
+
+            if(disc > 0)
+            {
+                x1 = (-b + Math.Sqrt(disc))/ 2 * a;
+                x2 = (-b - Math.Sqrt(disc))/ 2 * a;
+                Console.WriteLine("X1: " + x1);
+                Console.WriteLine("X2: " + x2);
+            }
+            else
+            {
+                if (disc == 0)
+                {
+                    x1 = -b / 2 * a;
+                    Console.WriteLine("X1: " + x1);
+                }else
+                {
+                    Console.WriteLine("No tiene soluciones");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
